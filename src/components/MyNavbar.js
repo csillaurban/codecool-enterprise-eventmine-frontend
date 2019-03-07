fd3c96e3d169da89113f7a2c3cfdbdb1a6a27de8
kemenykristof <kemenykristof24@gmail.com>
2019-03-07 18:02:17 +0100

    Add images)

---
+++ src/components/MyNavbar.js
@@ -0,0 +1,54 @@
+import React , { Component}  from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
+
+class MyNavbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">EventMine</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-l" navbar>
+                            <NavItem>
+                                <NavLink href="/components/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/reactstrap/reactstrap">Concerts</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/components/">Festivals</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/components/">Theatre</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default MyNavbar;
