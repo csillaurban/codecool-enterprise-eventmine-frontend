b52d2568f924c1e6d5a134eed29b1bd88456c5be
Tamas Frei <frei.tamas555@gmail.com>
2019-03-07 16:05:05 +0100

    Merge branch 'development' of https://github.com/csillaurban/codecool-enterprise-eventmine-frontend into development
    # Conflicts:
    #	src/components/ImageComp.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 import './App.css';
 import Header from "./components/Header";
 import EventList from "./components/EventList";
+import SearchResult from "./components/pages/SearchResult";
+import axios from 'axios';
 
 class App extends Component {
 
@@ -13,6 +16,54 @@ class App extends Component {
           </div>
         );
     }
+  state = {
+    events: [],
+    resultsFound: false
+  }
+
+  sendKeyword = (keyword) => {
+    axios.get(`http://localhost:8080/events/search/${keyword}`
+    ).then(res => this.setState({events: res.data, resultsFound: true}))
+  }
+
+  emptyResults = () => this.setState({resultsFound: false});
+
+
+  render() {
+    console.log(this.state.resultsFound);
+    if(this.state.resultsFound) {
+      return (
+          <Router>
+            <div className="App">
+              <div className="container">
+                <Header sendKeyword={this.sendKeyword}/>
+            <Redirect to="/events/results" render={props=> (
+              <React.Fragment>
+                <SearchResult events={this.state.events} emptyResults={this.emptyResults}/>
+              </React.Fragment>
+            )}/>
+              </div>
+            </div>
+          </Router>)
+    } else {
+      return (
+          <Router>
+            <div className="App">
+              <div className="container">
+                <Header sendKeyword={this.sendKeyword}/>
+                <Route path="/" render={props => (
+                    <React.Fragment>
+                        <EventList/>
+                    </React.Fragment>
+                )}/>
+
+              </div>
+            </div>
+          </Router>
+
+      )
+    }
+  }
 }
 
 export default App;
