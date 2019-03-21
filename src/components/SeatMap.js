d0293f7cc7e265c74fbef6e9bd17b36edba5b295
Tamas Frei <frei.tamas555@gmail.com>
2019-03-21 11:42:46 +0100

    Create SeatMap component

---
+++ src/components/SeatMap.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SeatMap extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default SeatMap;
