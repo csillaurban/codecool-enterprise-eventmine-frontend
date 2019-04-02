e2e7b00573ba420b68e545e29a72a01bcc3f8507
Csilla Urban <csillaurban@gmail.com>
2019-04-02 14:39:59 +0200

    import Auth

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import EventMine from "./containers/EventMine/EventMine";
+import Auth from './authService/Auth';
+
+
+const auth = new Auth();
+auth.login();
 
 class App extends Component {
+
   render() {
       return (
           <BrowserRouter>
@@ -16,3 +22,5 @@ class App extends Component {
 }
 
 export default App;
+
+
