cda3b95a62f528efad4f9e1127873d056de035bc
Csilla Urban <csillaurban@gmail.com>
2019-04-04 09:39:31 +0200

    redirect on Home navitem

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,12 +2,25 @@ import React, {Component} from 'react';
 import {Navbar, NavItem} from 'react-materialize';
 import './Navbar.css';
 
+
+
+
 class NavigationBar extends Component {
+
+    redirect = () => {
+        let path = "/home";
+        if(sessionStorage.getItem('accessToken')) {
+            this.props.history.push(path);
+        } else {
+            this.props.history.push("/");
+        }
+    }
+
     render () {
         return (
             <div className="navbar">
                 <Navbar brand={<a/>} alignLinks="left">
-                    <NavItem href={"/home"} >
+                    <NavItem onclick={this.redirect}>
                         Home
                     </NavItem>
                     <NavItem href="/">
